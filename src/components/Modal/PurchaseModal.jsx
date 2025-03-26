268cca373bf10c554261791eb3e31b3ac1a46147
Hera <mostafizhera@gmail.com>
2025-03-27 00:20:45 +0600

    post the orders data in db

--- src/components/Modal/PurchaseModal.jsx
+++ src/components/Modal/PurchaseModal.jsx
@@ -6,13 +6,74 @@ import {
   DialogPanel,
   DialogTitle,
 } from '@headlessui/react'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import useAuth from '../../hooks/useAuth'
+import toast from 'react-hot-toast';
+import { axiosSecure } from '../../hooks/useAxiosSecure';
 
 const PurchaseModal = ({ closeModal, isOpen, data }) => {
   // Total Price Calculation
   const { user } = useAuth();
   const { name, price, quantity, category, imageUrl, sellerInfo, description } = data
+  const [ordersAddress, setOrdersAddress] = useState('');
+  const [orderQuantity, setOrderQuantity] = useState(1);
+  const [plantsPrice, setPlantsPrice] = useState(0);
+
+
+  // Which the order quantity ================>>>>>>>>>>>>
+  
+  const handleQuantity = value => {
+    if (value > quantity) {
+      toast.error("Insufficient quantity!");
+      return;
+    }
+    if (value < 1) {
+      setOrderQuantity(1)
+      toast.error("Minimum order quantity is 1!");
+      return;
+    }
+
+    setOrderQuantity(value);
+    setPlantsPrice(price * value)
+    setOrdersInfo(prev => {
+      return { ...prev, totalPrice: price * value, quantity: value }
+    })
+  }
+
+  
+  // if the user is true then i get user information and set post the database
+
+  const [ordersInfo, setOrdersInfo] = useState({
+    userInfo: {
+      name: user?.displayName,
+      email: user?.email,
+      photo: user?.photoURL
+    },
+    name,
+    category,
+    imageUrl,
+    sellerInfo: sellerInfo,
+    address: ordersAddress,
+    totalPrice: price,
+    quantity: orderQuantity,
+    status: 'pending'
+  })
+
+
+  // post the orders data in database==============>>>>>>>>>>>>>>>>>>>>
+
+  const handlePurchase = async () => {
+    console.log(ordersInfo);
+    try{
+      await axiosSecure.post('/order', ordersInfo)
+      toast.success("Your order has been successfully!");
+    }catch(err){
+      console.error(err)
+      toast.error("Failed to purchase!");
+    }finally{
+      closeModal();
+    }
+  }
 
 
   return (
@@ -46,7 +107,7 @@ const PurchaseModal = ({ closeModal, isOpen, data }) => {
                   as='h3'
                   className='text-lg font-medium text-center leading-6 text-gray-900'
                 >
-                  Review Info Before Purchase
+                  Orders Froms
                 </DialogTitle>
                 <div className='mt-2'>
                   <p className='text-sm text-gray-500'>Plant: {name}</p>
@@ -71,7 +132,8 @@ const PurchaseModal = ({ closeModal, isOpen, data }) => {
                     className='w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
                     name='price'
                     type='number'
-                    placeholder='0'
+                    onChange={(e) => handleQuantity(e.target.value)}
+                    defaultValue={orderQuantity}
                     required
                     max={quantity}
                   />
@@ -79,11 +141,14 @@ const PurchaseModal = ({ closeModal, isOpen, data }) => {
                 {/* Shiping address....... */}
 
                 <div className='mt-2 flex items-center space-x-2'>
-                  <p className='text-sm text-gray-500'>Quantity:</p>
+                  <p className='text-sm text-gray-500'>Address:</p>
                   <input
                     className='w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
                     name='address'
                     type='text'
+                    onChange={(e) => setOrdersInfo(prev => {
+                      return { ...prev, address: e.target.value }
+                    })}
                     placeholder='Shiping address...'
                     required
                     max={quantity}
@@ -93,7 +158,7 @@ const PurchaseModal = ({ closeModal, isOpen, data }) => {
                 {/* Confirm order button */}
 
                 <div className='mt-5 text-end'>
-                  <button className=' btn btn-neutral text-white'>Confirm Order</button>
+                  <button onClick={handlePurchase} className=' btn btn-neutral text-white w-full'>{plantsPrice > 0 ? `$ ${plantsPrice}` : ''} Pay Now</button>
                 </div>
 
               </DialogPanel>
